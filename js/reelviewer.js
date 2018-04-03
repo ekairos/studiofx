128d7212f9876de4cd020db57c31665c4b389c44
Gareths13 <gareth.gs13@gmail.com>
2018-04-03 12:10:36 +0200

    demoReel viewer responsiveness
    Implement positioning and resizing video display.
    update reelviewer.js and studioStyle.css

--- js/reelviewer.js
+++ js/reelviewer.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
 
+	function positionViewer() {
+		var winHeight = $(window).height();
+		var winWidth = $(window).width();
+	//	console.log('win height = ' + winHeight);
+	//	console.log(winWidth);
+
+		var vidMaxHeight = $(window).width() / 1.77;
+
+		var diffPos = (winHeight - vidMaxHeight) / 2;
+	//	console.log('diffPosition = ' + parseInt(diffPos));
+
+			// positioning
+		var topPos = parseInt(diffPos < 0 ? 0 : diffPos);
+	//	console.log("top Position = " + topPos);
+		$('#viewer').css('top', topPos + 'px');
+	
+			// max sizes
+		var maxWidth = Math.round((winHeight * 1.77));
+		$('#viewer').css("max-width", maxWidth + 'px');
+	};
+	//	positiong for first modal drop
+	positionViewer();
+
+	//	resize and scale for window resizing
+	$(window).on('resize', positionViewer);
+
 	// reel viewer
 	$('#viewer').on('hidden.bs.modal', function() {
 		var reel = document.getElementById("heroVid");
