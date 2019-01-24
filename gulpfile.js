485109197fce3a188d6018dc22c148a62764f1b9
Gareths3 <gareth.gs13@gmail.com>
2019-01-24 12:18:47 +0100

    CSS Min
    directories structure change; minify css using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
 const { series } = require('gulp');
 const { src, dest } = require('gulp');
 const gulp = require('gulp');
-
+// js files requireemnts
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
-
 // for angularjs scripts
 const ngAnnotate = require('gulp-ng-annotate');
-
 // for unit test
 const Server = require('karma').Server;
+// css min requirements
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('autoprefixer');
+const cssmin = require('gulp-cssmin');
 
 // run unit tests from karma.conf.js
 function unitTestTask(done) {
@@ -26,7 +29,7 @@ function concatNgTask() {
   .pipe(rename({ extname: '.ant.js' }))
   .pipe(gulp.dest('js/gulp/annotated/'))
   .pipe(concat('js/studioapp.js'))
-  .pipe(gulp.dest('./'))
+  .pipe(gulp.dest('./'));
 }
 
 // minify JS files
@@ -34,8 +37,21 @@ function uglifyTask() {
   return src(['js/studioapp.js', 'js/gmapload.js', 'js/reelviewer.js', 'js/scrollspy.js'])
   .pipe(uglify())
   .pipe(rename({ extname: '.min.js' }))
-  .pipe(gulp.dest('dist/js/'))
+  .pipe(gulp.dest('dist/js/'));
+}
+
+// css minify series
+function cssMinifyTask() {
+  return src(['./src/css/resetCSS.css', './src/css/studioanimation.css', './src/css/studiostyle.css'])
+  .pipe(sourcemaps.init())
+  .pipe(postcss([ autoprefixer() ]))
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('./src/css/prfx'))
+  .pipe(cssmin())
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest('./dist/css'));
 }
 
 exports.unitTest = unitTestTask;
 exports.buildjs = series(concatNgTask, uglifyTask);
+exports.cssmin = cssMinifyTask;
